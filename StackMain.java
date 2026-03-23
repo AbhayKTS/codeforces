e1900b7418017c7d0995f73c781897b7cde4b8f2
Chaos_Immortal <abhay88998@gmail.com>
2026-03-23 15:02:14 +0530

    removing capacity variable as arraylist is used which can grow dynamically.

--- StackMain.java
+++ StackMain.java
@@ -2,43 +2,36 @@ import java.util.*;
 class Mystack{
     private ArrayList<Integer> arr;
     private int top;
-    int capacity;
-    public Mystack(int capacity){
-        this.capacity=capacity;
-        arr=new ArrayList<>(capacity);
+    public Mystack(){
+        arr=new ArrayList<>();
         top=-1;
     }
-    public void push(int item){
-        if(top==capacity-1){
-            System.out.println("Stack is full");
-            return;
-        }
-        top++;
+    public void push(Integer item){
         arr.add(item);
     }
-    public int pop(){
+    public Integer pop(){
         if(isEmpty()){
             System.out.println("Stack is empty");
             return -1;
         }
-        int res=arr.get(top);
-        top--;
+        int res=arr.get(arr.size()-1);
+        arr.remove(arr.size()-1);
         return res;
     }
-    public int peek(){
+    public Integer peek(){
         if(isEmpty()){
             System.out.println("Stack is empty");
             return -1;
         }
-        return arr.get(top);
+        return arr.get(arr.size()-1);
     }
     public boolean isEmpty(){
-        return top==-1;
+        return arr.isEmpty();
     }
 }
 public class StackMain{
     public static void main(String[] args){
-        Mystack s=new Mystack(5);
+        Mystack s=new Mystack();
         System.out.println(s.pop());
         s.push(10);
         s.push(20);
